09699a76c7c848b479fddffb7ee45e029c33c14e
orenbenkiki <oren@ben-kiki.org>
2022-12-28 17:04:09 +0200

    Add long description content type.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(  #
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords="ninja,make",
     name="ningen",
